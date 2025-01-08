fed6deefc004fb504def06bd7baf87e9aabb3766
Aman Prajapati <ap757162@gmail.com>
2025-01-08 11:14:17 +0530

    Day1-Basic Programme added

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+			public static void main(String args[]){
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter radius: ");
+			int radius = sc.nextInt();
+			int pi= 22/7;
+			int area= pi*radius*radius;
+			System.out.println("Area: "+ area);
+				}
+		}
+		
+		
